198fab08e3e6efc5a0844a0e2911a13fc7594056
Lorenzo <l.bartolini02@gmail.com>
2025-03-28 20:14:46 +0100

    Starting window manager on VNC

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-Xvfb :99 -screen 0 1024x768x16 &
+Xvfb :99 -screen 0 1920x1080x16 &
 export DISPLAY=:99
+fluxbox &
 x11vnc -display :99 -forever -nopw -rfbport 6123 &
+
+echo "Started VNC"
 
 mobius
